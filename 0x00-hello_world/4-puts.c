85e2b008f658185579519a4cdc3ed47c99926768
Sherry680 <faithnjeri506@gmail.com>
2022-09-08 09:45:37 -0700

    Print as shown

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>                                                                                                                      
+                                                                                                                                        
+/**                                                                                                                                     
+ *  * main - print in the put function                                                                                                     
+ *   *                                                                                                                                      
+ *    *Description: using the main function                                                                                                  
+ *     *this program prints "Programming is like bouilding puzzle"                                                                            
+ *      * Return: 0                                                                                                                            
+ *       */                                                                                                                                     
+int main(void)                                                                                                                          
+	                                                                                                                                        
+{                                                                                                                                       
+	                                                                                                                                        
+	puts("\"Programming is like building a multilingual puzzle");                                                                           
+	                                                                                                                                        
+	return (0);                                                                                                                             
+	                                                                                                                                        
+}
