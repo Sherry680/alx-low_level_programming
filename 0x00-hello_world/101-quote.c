56d102a6d0f4679f8e306d3d28e14f649f43ea7f
Sherry680 <faithnjeri506@gmail.com>
2022-09-11 07:04:45 -0700

    Print the exact statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
-
-#include <unistd.h>
-
+#include <unisd.h>
 /**
+ * main - main functionin the task
  *
- *   * main - main function in the task
- *
- *     *
- *
- *       *
- *
- *         *Return: return 1
- *
- *           */
-
+ * Return: return 1
+ */
 int main(void)
-
 {
-
-			write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-
-						return (1);
-
+	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	return (1);
 }
