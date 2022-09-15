5b652d134a05f1e441455f4b9b77f83fbe09b130
Sherry680 <faithnjeri506@gmail.com>
2022-09-15 13:47:47 -0700

    Multiples between 0 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - multiples of 3 or 6
+ *
+ * Description: multiples between 0 and 100
+ * Return: always (0)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 99; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ";
+					else
+					{
+					printf("%a ", a);
+					}
+					}
+					printf(Buzz\n");
+					return (0);
+					}
