bb96a7d2f3092942c9cd63b491f558b9a5f25646
Sherry680 <faithnjeri506@gmail.com>
2022-09-19 12:43:21 -0700

    Print character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - print character
+ * @str: input string
+ *
+ */
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+			_putchar (str[a]);
+	}
+	_putchar ('\n');
+}
