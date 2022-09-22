08541981aaf8b2ef67f727b1c47387ba2d1a6b1a
Sherry680 <faithnjeri506@gmail.com>
2022-09-22 11:37:38 -0700

    README.md file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the string that will be appended
+ * Return: pointer to the resulting string
+ */
+
+char *-strcat(char *dest, char *src)
+{
+	int len = 0, a;
+
+	while (dest[len])
+		len++;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[len] = src[a];
+		len += 1;
+
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
