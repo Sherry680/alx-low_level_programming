5985a90ea1b837df64f59bc0c5d2e19a84d47d51
Sherry680 <faithnjeri506@gmail.com>
2022-09-23 11:11:05 -0700

    print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,28 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int a, b, count;
+	unsigned int number = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		a = n * -1;
-	}
-	else
-	{
-		a = n;
+		_putchar('-');
+		number = number;
 	}
 
-	b = a;
-	count = 1;
+	if ((number / 10) > 0)
+		print_number(number / 10);
 
-	while (b > 9)
-	{
-		b /= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((a / count) % 10) + 48);
-	}
+	_putchar((number % 10) + '0');
 }
