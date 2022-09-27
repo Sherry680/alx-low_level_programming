358286892b99e641e737d4d51613b27a5fad5d58
Sherry680 <faithnjeri506@gmail.com>
2022-09-27 12:51:54 -0700

    Search a string for a set of bytes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,18 +13,17 @@ unsigned int _strspn(char *s, char *accept)
 	while (accept[a])
 	{
 		b = 0;
+
 		while (s[b] != 32)
 		{
-			if (accept[a] == s[b]
-					{
-					c++;
-					}
+			if (accept[a] == s[b])
+			{
+				c++;
+			}
 
-					b++;
-					}
-
-					a++;
-					}
-
-					return (c);
-					}
+			b ==;
+		}
+		a++;
+	}
+	return (c);
+}
