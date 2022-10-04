f9ba80953ae5c526f74cd0ea390bc2ceff0f7963
Sherry680 <faithnjeri506@gmail.com>
2022-10-04 11:29:42 -0700

    Concatenate all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,7 +16,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0, total = 0; i < ac; i++)
 	{
-		for (j = 0; *(*(av + i) + j) != '\0'; j++; total++)
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
 			;
 		total++;
 	}
